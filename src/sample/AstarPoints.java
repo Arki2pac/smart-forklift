f956fa1bf852156829b9da0e891f91f8c10ec8e1
Arkadiusz Chrobak <ac93912@st.amu.ede.pl>
2016-05-09 12:23:55 +0200

    Updatet directory settings

---
+++ src/sample/AstarPoints.java
@@ -0,0 +1,30 @@
+package sample;
+
+/**
+ * Created by Arkadiusz Chrobak on 08.05.2016.
+ */
+public class AstarPoints {
+    private int X;
+    private int Y;
+
+    public AstarPoints() {
+        this(0,0);
+    }
+    public AstarPoints(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+    public int getX() {
+        return X;
+    }
+    public int getY() {
+        return Y;
+    }
+    // public String getXY() { return (X + ";" + Y); }
+    public void setX(int X) {
+        this.X = X;
+    }
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+}
